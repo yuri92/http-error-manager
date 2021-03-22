8fa708dede2f4db683ee080b6edb6d77acedf917
Yuri Simeone <y.simeone@nesea.it>
2021-03-22 12:44:43 +0100

    Aggiornato export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 import { HttpErrorProcessor } from "./models/http-error-processor";
-import { IHttpErrorConfig } from "./models/http-error-config.interface";
-export { HttpErrorProcessor, IHttpErrorConfig }
+import { IHttpErrorConfig, IHttpErrorModal } from "./models/http-error-config.interface";
+export { HttpErrorProcessor, IHttpErrorConfig, IHttpErrorModal }
